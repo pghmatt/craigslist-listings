ced5c496312c6ff390f7c989e2aa1b9e8f489017
Dave Idol <david.idol@gmail.com>
2014-04-10 16:43:10 -0700

    It's working!

---
+++ README.md
@@ -0,0 +1,43 @@
+
+# craigslist-account
+
+A tool for managing your account and listings on Craigslist.
+
+Tired of having to remember to log in every 48 hours just so you can click the "renew" button for your listing? Why not use a tool to do it for you?
+
+## Introduction
+
+```js
+var craigslist = require('craigslist-account');
+
+craigslist.renewListings();
+```
+
+## Installation
+`npm install craigslist-account`
+
+
+## License
+
+(The MIT License)
+
+Copyright (c) 2014 David Idol &lt;david.idol@gmail.com&gt;
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- craigslist.js
+++ craigslist.js
@@ -1,17 +1,11 @@
-var cheerio = require('cheerio');
-var request = require('request');
-var clLogin = require('./craigslist.login.js');
+var cheerio = require("cheerio");
+var request = require("request");
+var clLogin = require("./craigslist.login.js");
 
 var email = clLogin.email;
 var password = clLogin.password;
 
 var cookieJar = request.jar(); // enable cookies
-request.defaults({
-	headers: {
-		// spoof the user agent
-		'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1664.3 Safari/537.36'
-	}
-});
 
 function createFormObject($, formDom) {
 	var formObject = {};
@@ -34,15 +28,17 @@ exports.renewListings = function(callback, isRetry) {
 			jar: cookieJar
 		},
 		function(err, response, body) {
-			if (err) return next(err);
-			var $ = cheerio.load(body);
+			if (err) {
+				console.log(err);
+				return callback(false);
+			}
 
-			console.log('Cookie jar:', cookieJar);
+			var $ = cheerio.load(body);
 
 			var loginForm = $("form[name='login']");
 
 			var notLoggedIn = !!($("body.login").length);
-			console.log("- Not logged in?", !!notLoggedIn);
+			console.log("- Not logged in?", notLoggedIn);
 
 			console.log("- Account page?", !!($("body.account").length));
 
@@ -63,52 +59,60 @@ exports.renewListings = function(callback, isRetry) {
 				var formObject = createFormObject($, form);
 
 				console.log("- Submitting login form with object:", formObject);
-				request.post(form.attr("action"), { form: formObject, followAllRedirects: true },
+				request.post(form.attr("action"), { form: formObject, jar: cookieJar },
 					function(err, response, body) {
-						if(response.statusCode >= 200 && response.statusCode < 300){
-							console.log('- Successfully logged in ('+response.statusCode+'). Cookie jar:', cookieJar);
+						if (!err && response.statusCode === 302) {
+							console.log("- Successfully logged in ("+response.statusCode+").");
 
-							exports.renewListings(function(){}, true); // request the page again now that we are logged in
+							exports.renewListings(callback, true); // request the page again now that we are logged in
 						} else {
-							console.log('- Error submitting login form: '+ response.statusCode);
+							console.log("- Error submitting login form: "+ response.statusCode);
 							console.log(body);
 							return callback(false);
 						}
 					}
 				);
 			} else {
-				var postingsTable = $("table[summary='postings']");
-				var activeListings = postingsTable.children().first().children()
-										.filter(function(index) {
-											return !!($(this).find("td[class='status']:contains('Active')")[0]);
-										});
-				activeListings.each(function() {
-					var listingName = $(this).find("td[class='title']:first").children().first().text();
-					var renewForm = $(this).find("td[class='buttons']:first").find("form[method='POST']:contains('renew')").first();
+				var listings = $("table[summary='postings'] > tr");
+
+				console.log("- Total listings:", listings.length);
+
+				var activeListings = listings.filter(function(index) {
+					return $(this).find("td[class='status']:contains(Active)").length; // TODO use .text() to ignore case
+				});
+
+				console.log("- Active listings:", activeListings.length);
+				console.log();
+
+				activeListings.each(function(index) {
+					var listingName = $(this).find("td[class='title'] a").text().trim();
+					var renewForm = $(this).find("td[class='buttons']").find("form[method='POST']").filter(function(index) {
+						return $(this).find("input[type='submit'][value='renew']").length; // TODO ignore case
+					});
+
 					if (renewForm.length) {
 						var formObject = createFormObject($, renewForm);
 						console.log("- Submitting renewal form with object:", formObject);
-						/*
-						request.post(form.attr("action"), { form: formObject, followAllRedirects: true },
+						request.post(renewForm.attr("action"), { form: formObject, jar: cookieJar, followAllRedirects: true },
 							function(err, response, body) {
-								if (response.statusCode >= 200 && response.statusCode < 300){
-									
+								if (!err && response.statusCode >= 200 && response.statusCode < 300){
+									console.log('Successfully renewed listing "'+ listingName + '"');
 									console.log();
-									console.log('Successfully renewed listing '+ listingName);
-									console.log();
-									// if last one return callback(true); // TODO use async lib here
 								} else {
-									console.log('- Error submitting renewal form: '+ response.statusCode);
+									console.log("- Error submitting renewal form: "+ response.statusCode);
 									console.log(body);
 									return callback(false);
 								}
 							}
 						);
-						*/
 					} else {
+						console.log('Listing "'+ listingName +'" is not ready to be renewed');
 						console.log();
-						console.log('Listing '+ listingName +' is not ready to be renewed');
-						console.log();
+					}
+
+					if (index == activeListings.length - 1) {
+						// We are done
+						return callback(true); 
 					}
 				});
 			}
